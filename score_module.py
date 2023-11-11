08a93d87502ccdc99f1399c3f9fe40d716644e7a
thiago-Coderr <tiagoafonso110@gmail.com>
2023-11-11 17:41:37 +0530

    newly created functions for score display purpose

--- score_module.py
+++ score_module.py
@@ -52,3 +52,60 @@ def sort_scores(file_name):
             file.write(f"{scores},{names}\n")
 
 
+def ranking_updation(file_name, player_name, player_score):
+    # Add the last player details to the file
+    append_content(file_name, player_name, player_score)
+
+
+def print_rank(f_pen, file_name):
+    array_name = []
+    array_score = []
+    aux_name = []
+
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+    for line in lines:
+        sides = line.split(",")
+        array_score.append(float(sides[0]))
+        array_name.append(sides[1])
+
+    for name in array_name:
+        aux_name.append(name.replace("\n", ""))
+
+    f_pen.penup()
+    f_pen.goto(-480, 200)
+    f_pen.pendown()
+
+    f_pen.write("============ Score Ranking ===========", align="left", font=("Arial", 24, "bold"))
+    f_pen.goto(-480, 110)
+    f_pen.pendown()
+    f_pen.write("Rank 1", align="left", font=("Arial", 14))
+    f_pen.goto(-480, 90)
+    f_pen.pendown()
+    f_pen.write(f"Name: {aux_name[0]} ", align="left", font=("Arial", 14))
+    f_pen.goto(-480, 70)
+    f_pen.pendown()
+    f_pen.write(f"Score: {array_score[0]}", align="left", font=("Arial", 14))
+
+    f_pen.goto(-480, 30)
+    f_pen.pendown()
+    f_pen.write("Rank 2.", align="left", font=("Arial", 14))
+    f_pen.goto(-480, 10)
+    f_pen.pendown()
+    f_pen.write(f"Name: {aux_name[1]}", align="left", font=("Arial", 14))
+    f_pen.goto(-480, -10)
+    f_pen.pendown()
+    f_pen.write(f"Score: {array_score[1]}", align="left", font=("Arial", 14))
+
+    f_pen.goto(-480, -50)
+    f_pen.pendown()
+    f_pen.write("Rank 3.", align="left", font=("Arial", 14))
+    f_pen.goto(-480, -70)
+    f_pen.pendown()
+    f_pen.write(f"Name: {aux_name[2]}", align="left", font=("Arial", 14))
+    f_pen.goto(-480, -90)
+    f_pen.pendown()
+    f_pen.write(f"Score: {array_score[2]}", align="left", font=("Arial", 14))
+
+
+
